15820ac812947fe5439358b9db6900f690577407
Liz Dobbins <eldobbins@alaska.edu>
2021-11-15 13:38:20 -0900

    starting generalization process

--- tsg/preprocess/clean_TSG.sh
+++ tsg/preprocess/clean_TSG.sh
@@ -1,35 +1,44 @@
 #!/bin/sh
+
+# cleanup from prior runs
 rm nuq_nav.dat nuq_tsg.dat
+
+
 #loop through all the files and cut out just the best parts
-for file in ../data/Level_0/NUQ202001P/SBE45-TSG/tsg.*;
+
+cruise_id=NUQ202001P
+for file in ../data/Level_0/$cruise_id/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
-		./fix_tsg.awk $file >> nuq_tsg.dat	
+		./config/$cruise_id/tsg_parse.awk $file >> nuq_tsg.dat	
 	done
-for file in ../data/Level_0/NUQ202001P/Navigation/nav.*;
+for file in ../data/Level_0/$cruise_id/Navigation/nav.*;
 	do
 		echo "$file";
-		./fix_nav.awk $file >> nuq_nav.dat	
+		./config/$cruise_id/nav_parse.awk $file >> nuq_nav.dat	
 	done
-#	
-for file in ../data/Level_0/NUG202003S/SBE45-TSG/tsg.*;
+
+
+cruise_id=NUG202003S	
+for file in ../data/Level_0/$cruise_id/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
-		./fix_tsg.awk $file >> nuq_tsg.dat	
+		./config/$cruise_id/tsg_parse.awk $file >> nuq_tsg.dat	
 	done
-for file in ../data/Level_0/NUG202003S/Navigation/nav.*;
+for file in ../data/Level_0/$cruise_id/Navigation/nav.*;
 	do
 		echo "$file";
-		./fix_nav.awk $file >> nuq_nav.dat	
+		./config/$cruise_id/nav_parse.awk $file >> nuq_nav.dat	
 	done	
-#	
-for file in ../data/Level_0/NUQ202003S/SBE45-TSG/tsg.*;
+
+cruise_id=NUQ202003S	
+for file in ../data/Level_0/$cruise_id/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
-		./fix_tsg.awk $file >> nuq_tsg.dat	
+		./config/$cruise_id/tsg_parse.awk $file >> nuq_tsg.dat	
 	done
-for file in ../data/Level_0/NUQ202003S/Navigation/nav.*;
+for file in ../data/Level_0/$cruise_id/Navigation/nav.*;
 	do
 		echo "$file";
-		./fix_nav.awk $file >> nuq_nav.dat	
+		./config/$cruise_id/nav_parse.awk $file >> nuq_nav.dat	
 	done
