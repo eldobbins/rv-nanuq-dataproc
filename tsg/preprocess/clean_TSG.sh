be60caa5b3849281a330496a22bd8c00d6f8c36e
Vlad Dracula <vlad@tran.sylvnia.ia>
2021-11-15 13:07:54 -0900

    running on laptop

--- tsg/preprocess/clean_TSG.sh
+++ tsg/preprocess/clean_TSG.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 rm nuq_nav.dat nuq_tsg.dat
 #loop through all the files and cut out just the best parts
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUQ202001P/SBE45-TSG/tsg.*;
+for file in ../data/Level_0/NUQ202001P/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
 		./fix_tsg.awk $file >> nuq_tsg.dat	
 	done
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUQ202001P/Navigation/nav.*;
+for file in ../data/Level_0/NUQ202001P/Navigation/nav.*;
 	do
 		echo "$file";
 		./fix_nav.awk $file >> nuq_nav.dat	
 	done
 #	
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUG202003S/SBE45-TSG/tsg.*;
+for file in ../data/Level_0/NUG202003S/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
 		./fix_tsg.awk $file >> nuq_tsg.dat	
 	done
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUG202003S/Navigation/nav.*;
+for file in ../data/Level_0/NUG202003S/Navigation/nav.*;
 	do
 		echo "$file";
 		./fix_nav.awk $file >> nuq_nav.dat	
 	done	
 #	
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUQ202003S/SBE45-TSG/tsg.*;
+for file in ../data/Level_0/NUQ202003S/SBE45-TSG/tsg.*;
 	do
 		echo "$file";
 		./fix_tsg.awk $file >> nuq_tsg.dat	
 	done
-for file in /Users/hstats/Documents/LTER/NANUQ2020/DATA/TSG_DAQ/NUQ202003S/Navigation/nav.*;
+for file in ../data/Level_0/NUQ202003S/Navigation/nav.*;
 	do
 		echo "$file";
 		./fix_nav.awk $file >> nuq_nav.dat	
