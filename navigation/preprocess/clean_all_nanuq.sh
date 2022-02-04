cf08c987ac19467452411e38551ac566e4010f70
Liz Dobbins <eldobbins@alaska.edu>
2022-02-04 11:35:00 -0900

    preprocess navigation is running

---
+++ navigation/preprocess/clean_all_nanuq.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Process all the navigation data for Nanuq cruises.
+# 
+# Loop through all the cruises and call the script that concatenates the little files 
+# together.
+#
+# ELD
+# 2/4/2022
+
+indir=../../data/Navigation/Level_0/${cruise_id}
+
+# call the processing script for each cruise
+for cruise in `ls ${indir}`;
+	do
+		echo "$cruise";
+		./clean_nanuq_cruise.sh ${cruise}
+	done
---
+++ navigation/preprocess/clean_nanuq_cruise.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# Concatinate multiple navigation files into a single file per cruise.
+# 
+# Loop through all the files. Reformat the latitude/longitude degrees and minutes
+# into separate columns. Resulting files contain the same data (still Level 0)
+# but in a new format (v2).
+#
+# If the format changes, you'll need to revise the awk script.
+#
+# ELD
+# 2/4/2022
+# Original code developed by Hank Statscewich
+#
+
+cruise_id=$1
+
+indir=../../data/Navigation/Level_0/${cruise_id}
+outdir=../../data/Navigation/Level_0_v2
+
+
+# remove previous work if it exists
+outfile=${outdir}/uaf_${cruise_id}_NAV_L0_v2.dat
+\rm ${outfile}
+
+
+# add a header
+echo "#Year Month Day Hour Minute Second Lat_Deg Lat_Min Lon_Deg Lon_Min Heading COG SOG" > ${outfile}
+
+# parse each incoming Navigation file and add it to the outgoing file
+for file in ${indir}/nav.*;
+	do
+		echo "$file";
+		./nav_parse.awk $file >> ${outfile}
+	done
+
